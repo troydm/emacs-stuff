e4f68a55aeddc188bde4a947cbca5b096ab53aec
troydm <dmitry_627@mail.ru>
2011-05-27 16:24:36 +0400

    added copyright to windcycle

--- windcycle.el
+++ windcycle.el
@@ -1,3 +1,27 @@
+;;; windcycle.el --- Window cycling for Emacs
+
+;; Copyright (C) 2011 Dmitry Geurkov
+
+;; Author: Dmitry Geurkov <dmitry_627@mail.ru>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;; Simple Window cycling for Emacs using C-x direction arrows
+;;; To use add 
+;;; (require 'windcycle) 
+;;; to your .emacs
 
 ;; Windows Cycling
 (defun windmove-up-cycle()
@@ -28,3 +52,5 @@
 (global-set-key (kbd "C-x <down>") 'windmove-down-cycle)
 (global-set-key (kbd "C-x <right>") 'windmove-right-cycle)
 (global-set-key (kbd "C-x <left>") 'windmove-left-cycle)
+
+(provide 'windcycle)
